f40486ef4b0f9b471bf474ae9944a28a7e7aea6b
HEMMO0208 <hmkang@kaist.ac.kr>
2026-05-21 01:58:37 +0000

    Change save_steps from 1000 to 500 in nextqa scripts

--- scripts/nextqa/finetune_nextqa_diffusion.sh
+++ scripts/nextqa/finetune_nextqa_diffusion.sh
@@ -67,7 +67,7 @@ torchrun --nnodes "$WORLD_SIZE" \
     --gradient_accumulation_steps "$GRADIENT_ACCUMULATION_STEPS" \
     --eval_strategy "no" \
     --save_strategy "steps" \
-    --save_steps 1000 \
+    --save_steps 500 \
     --save_total_limit 2 \
     --llm_lr 1e-5 \
     --mm_projector_lr 1e-5 \
